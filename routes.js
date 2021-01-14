76dbe3fbe4f0c83d757abd813b5dae971fc0a379
Chris L <11223573+sirchras@users.noreply.github.com>
2021-01-15 12:06:36 +1300

    Update root route

--- routes.js
+++ routes.js
@@ -1,10 +1,20 @@
-const express = require('express')
 const path = require('path')
+const express = require('express')
+
+const utils = require('./utils')
 
 const router = express.Router()
 
 module.exports = router
 
+const filePath = path.join(__dirname, 'pets.json')
+
 router.get('/', (req, res) => {
-  res.render('home')
+  utils.getViewData(filePath, (err, viewData) => {
+    if(err) { 
+      res.status(500).send(err.message)
+    } else {
+      res.render('home', viewData)
+    }
+  })
 })
