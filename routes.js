1985cf62369add3a5ecc81cadec694b575852c98
Tausani Ah Chong <tausani93@gmail.com>
2021-01-16 20:45:51 +1300

    edit form to POST req.body to route

--- routes.js
+++ routes.js
@@ -7,6 +7,10 @@ const router = express.Router()
 
 module.exports = router
 
+router.use(express.urlencoded({
+  extended: false
+}))
+
 const filePath = path.join(__dirname, 'pets.json')
 
 router.get('/', (req, res) => {
@@ -37,23 +41,25 @@ router.get('/new', (req, res) => {
 })
 
 
-// router.post('/new', (req, res) => {
-//   utils.getViewData(filePath, (err, viewData) => {
-//     if (err) {
-//       res.status(500).send(err.message) 
-//     } else {
-//       const newObjPetData = req.body
-//       const newPetID = viewData.pets.length + 1
-//       newObjPetData.id = newPetID
-//       viewData.pets.push(newObjPetData)
+router.post('/new', (req, res) => {
+  // utils.getViewData(filePath, (err, viewData) => {
+  //   if (err) {
+  //     res.status(500).send(err.message) 
+  //   } else {
+      console.log(req.body)
+      res.redirect('new')
+      // const newObjPetData = req.body
+      // const newPetID = viewData.pets.length + 1
+      // newObjPetData.id = newPetID
+      // viewData.pets.push(newObjPetData)
 
-//       const newPetDataStr = JSON.stringify(viewData, null, 2)
+      // const newPetDataStr = JSON.stringify(viewData, null, 2)
 
-//       utils.writeNewData(filePath, newPetDataStr, (err, data) => {
-//         if(err) return res.status(500).send(err.message)
+      // utils.writeNewData(filePath, newPetDataStr, (err, data) => {
+      //   if(err) return res.status(500).send(err.message)
 
-//         res.redirect(`/pets/${newObjPetData.id}`)
-//       })
-//     }
-//   })
-// })
+      //   res.redirect(`/pets/${newObjPetData.id}`)
+      // })
+    // }
+  // })
+})
