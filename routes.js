2973bf420e4ebfc94e5a17f6292d9aeb08015a03
Tausani Ah Chong <tausani93@gmail.com>
2021-01-18 08:23:16 +1300

    add form writefile functionality

--- routes.js
+++ routes.js
@@ -11,10 +11,8 @@ router.use(express.urlencoded({
   extended: false
 }))
 
-const filePath = path.join(__dirname, 'pets.json')
-
 router.get('/', (req, res) => {
-  utils.getViewData(filePath, (err, viewData) => {
+  utils.getViewData((err, viewData) => {
     if(err) { 
       res.status(500).send(err.message)
     } else {
@@ -26,7 +24,7 @@ router.get('/', (req, res) => {
 router.get('/pets/:id', (req, res) => {
   const petId = Number(req.params.id)
 
-  utils.getViewData(filePath, (err, viewData) => {
+  utils.getViewData((err, viewData) => {
     if(err) { 
       res.status(500).send(err.message)
     } else {
@@ -42,24 +40,9 @@ router.get('/new', (req, res) => {
 
 
 router.post('/new', (req, res) => {
-  // utils.getViewData(filePath, (err, viewData) => {
-  //   if (err) {
-  //     res.status(500).send(err.message) 
-  //   } else {
-      console.log(req.body)
-      res.redirect('new')
-      // const newObjPetData = req.body
-      // const newPetID = viewData.pets.length + 1
-      // newObjPetData.id = newPetID
-      // viewData.pets.push(newObjPetData)
-
-      // const newPetDataStr = JSON.stringify(viewData, null, 2)
-
-      // utils.writeNewData(filePath, newPetDataStr, (err, data) => {
-      //   if(err) return res.status(500).send(err.message)
-
-      //   res.redirect(`/pets/${newObjPetData.id}`)
-      // })
-    // }
-  // })
+    const newObjPetData = req.body
+    utils.writeNewData(newObjPetData, (err) => {
+      if(err) return res.status(500).send(err.message)
+      res.redirect('/')
+    })
 })
--- utils.js
+++ utils.js
@@ -1,17 +1,18 @@
 const fs = require('fs')
+const path = require('path')
+
+const filePath = path.join(__dirname, 'pets.json')
 
 module.exports = {
   getViewData,
   writeNewData
 }
 
-function getViewData(filePath, callback) {
+function getViewData(callback) {
     fs.readFile(filePath, 'utf8', (err, contents) => {
       if(err) return callback(new Error('Unable to read file'))
-
       try {     
         const viewData = JSON.parse(contents) 
-
         callback(null, viewData)
       } catch(parseError) {
         callback(new Error('Unable to parse file'))
@@ -19,10 +20,18 @@ function getViewData(filePath, callback) {
     })
 }
 
-
-function writeNewData(filePath, petData, callback) {
-  fs.writeFile(filePath, petData, 'utf8', (err, contents) => {
-    if(err) return callback(new Error('Unable to write file'))
-    callback(null, contents)
+function writeNewData(newObjPetData, callback) {
+  fs.readFile(filePath, 'utf8', (err, contents) => {
+    if(err) return callback(new Error('Unable to read file'))
+    try {     
+      const viewData = JSON.parse(contents)
+      const newPetId = viewData.pets.length + 1
+      newObjPetData.id = newPetId
+      viewData.pets.push(newObjPetData)
+      const petDataStr = JSON.stringify(viewData, null, 2)
+      fs.writeFile(filePath, petDataStr, 'utf8', callback)
+    } catch(parseError) {
+      callback(new Error('Unable to stringify file'))
+    }
   })
 }
